fbb7a4f14b1e1d88e53d4040be36b489dd2f1957
oluoko <brianotieno586@gmail.com>
2025-02-23 14:13:26 +0300

    Oluoko's APIs: First Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Oluoko&apos;s APIs</h1>
+      <div className="">
+        A simple API for various purposes. <br />
+      </div>
+    </div>
+  );
+}
