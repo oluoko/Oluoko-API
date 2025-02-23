2f820cf7a08926679588982b652e2d5f15df7b60
oluoko <brianotieno586@gmail.com>
2025-02-23 14:31:11 +0300

    Oluoko Api: Updated the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl font-bold">Oluoko&apos;s APIs</h1>
-      <div className="">
-        A simple app that will be a server for API endpoints for various
-        purposes. <br />
-        Such as: <br />
-        <ul className="list-disc list-inside mr-4">
-          <li>
-            For the backend of mobile dev projects that are still in
-            development.
-          </li>
-          <li>
-            For the backend of web dev projects that are still in development.
-          </li>
+      <h1 className="text-4xl font-bold my-2">Oluoko&apos;s APIs</h1>
+      <Card className="w-[90vw] md:w-[70vw] lg:w-[50vw] text-lg text-center">
+        <CardContent>
+          A simple app that will be a server for API endpoints for various
+          purposes. <br />
+          Such as: <br />
+          <ul className="list-disc list-inside mr-4">
+            <li>
+              For the backend of mobile dev projects that are still in
+              development.
+            </li>
+            <li>
+              For the backend of web dev projects that are still in development.
+            </li>
 
-          <li>And many more...</li>
-        </ul>
-      </div>
+            <li>And many more...</li>
+          </ul>
+        </CardContent>
+      </Card>
     </div>
   );
 }
