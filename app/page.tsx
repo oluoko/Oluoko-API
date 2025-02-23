5ab7d4727f50a3d173fc8b0aca8de478a0949a5a
oluoko <brianotieno586@gmail.com>
2025-02-23 14:24:50 +0300

    Oluoko Api: Updated the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl font-bold">Oluoko&apos;s APIs</h1>
       <div className="">
-        A simple API for various purposes. <br />
+        A simple app that will be a server for API endpoints for various
+        purposes. <br />
+        Such as: <br />
+        <ul className="list-disc list-inside mr-4">
+          <li>
+            For the backend of mobile dev projects that are still in
+            development.
+          </li>
+          <li>
+            For the backend of web dev projects that are still in development.
+          </li>
+
+          <li>And many more...</li>
+        </ul>
       </div>
     </div>
   );
